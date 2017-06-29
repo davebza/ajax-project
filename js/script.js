1b3733876a646af4e7d948fbaa9fc4aa186e7bc0
Dave Brown <daveb.za@gmail.com>
2017-06-29 11:28:07 +0800

    FEAT: added wiki search for city

--- js/script.js
+++ js/script.js
@@ -12,7 +12,6 @@ function loadData() {
     $nytElem.text("");
 
     // load streetview
-
     // YOUR CODE GOES HERE!
     var street = $('#street').val();
     var city = $('#city').val();
@@ -20,6 +19,24 @@ function loadData() {
     //get Google street view pic:
     var imageUrl = "http://maps.googleapis.com/maps/api/streetview?size=600x300&location="+address;
     $body.append('<img class="bgimg" src="'+imageUrl+'">');
+
+    // Wiki Api request goes here:
+    var wikiUrl = "http://en.wikipedia.org/w/api.php?action=opensearch&search="+city+"&format=json&callback=wikiCallback";
+    $.ajax({
+        url: wikiUrl,
+        dataType: "jsonp",
+        // jsonp: this can be updated if needed
+        success: function(response){
+            var articleList = response[1];
+
+            for(var i = 0; i < articleList.length; i++){
+                articleStr = articleList[i];
+                var url = "http://en.wikipedia.org/wiki/"+articleStr;
+                $wikiElem.append('<li><a href="'+url+'" target="_blank">'+articleStr+'</a></li>');
+            };
+        }
+
+    });
 
     // get nyt article:
     // Built by LucyBot. www.lucybot.com
@@ -36,14 +53,15 @@ function loadData() {
       $nytHeaderElem.text('New York Times articles about '+address);
       for (var i = 0; i < articles.length; i++){
           var article = articles[i];
-          $nytElem.append('<li class="article">'+'<a href="'+article.web_url+'">'+article.headline.main+'</a><p>'+article.snippet+'</p>'+'</li>');
-
+          $nytElem.append('<li class="article">'+'<a href="'+article.web_url+'" target="_blank">'+article.headline.main+'</a><p>'+article.snippet+'</p>'+'</li>');
       }
   }).error(function(e) {
       $nytHeaderElem.text('New York Times Articles could not be found for this location.');
     });
-
     return false;
 };
+
+
+
 
 $('#form-container').submit(loadData);
